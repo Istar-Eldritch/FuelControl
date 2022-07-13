541040c3e787b7b429a22cd3e1f4adb244213a0a
Istar-Eldritch <me@ruben.io>
2022-07-13 17:48:54 +0100

    Normalise file names

--- Scripts/5_Mission/FuelControl/mission.c
+++ Scripts/5_Mission/FuelControl/mission.c
@@ -1,8 +1,6 @@
 
-modded class MissionBase
-{
-	void MissionBase()
-	{
+modded class MissionBase {
+	void MissionBase() {
 		Print("[FuelControl] Creating mission base");
 		FuelStationManager manager = GetFuelStationManager();
 		GetRPCManager().AddRPC("FuelControl", "GetFuelStations", manager, SingleplayerExecutionType.Both);
@@ -10,22 +8,6 @@ modded class MissionBase
 
 		if (GetGame().IsClient()) {
 			manager.SyncFuelStations();
-		}
-	}
-
-	void TestRPCFunction(CallType type, ParamsReadContext ctx, PlayerIdentity sender, Object target)
-	{
-		Param1<string> data;
-		if (!ctx.Read(data)) return;
-
-		if (type == CallType.Server)
-		{
-			Print("Function called on server!");
-			GetRPCManager().SendRPC("FuelControl", "TestRPCFunction", new Param1<string>("Hello, World!"));  
-		}
-		else
-		{
-			Print("Function called on client!");
 		}
 	}
 
