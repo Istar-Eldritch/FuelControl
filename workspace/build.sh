2a108295d705cdb7e336eaca0c86620a4bdcb813
Ruben Paz <me@ruben.io>
2023-11-27 00:37:18 +0000

    Include all files on build

--- workspace/build.sh
+++ workspace/build.sh
@@ -5,12 +5,18 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WIN_PROGRAMS_PATH=$(wslvar "programfiles(x86)")
 WIN_DZ_TOOL_PATH="$WIN_PROGRAMS_PATH\Steam\steamapps\common\DayZ Tools\Bin\AddonBuilder"
 
-WIN_FC_ADDONS_PATH="$(wslvar "userprofile")\Documents\DayZData\Mods\@FuelControl\addons"
-FC_ADDONS_PATH=$(wslpath "$WIN_FC_ADDONS_PATH")
+WIN_FC_PATH="$(wslvar "userprofile")\Documents\DayZData\Mods\@FuelControl"
+FC_PATH=$(wslpath "$WIN_FC_PATH")
 
-echo $FC_ADDONS_PATH
-rm -fr "$FC_ADDONS_PATH"
-mkdir -p "$FC_ADDONS_PATH"
+echo $FC_PATH
+rm -fr "$FC_PATH"
+mkdir -p "$FC_PATH/addons"
+cp -r "$SCRIPT_DIR/../data" "$FC_PATH/data"
+cp -r "$SCRIPT_DIR/../profiles" "$FC_PATH/profiles"
+cp -r "$SCRIPT_DIR/../scripts" "$FC_PATH/src"
+cp -r "$SCRIPT_DIR/../LICENSE" "$FC_PATH/LICENSE"
+cp -r "$SCRIPT_DIR/../README.md" "$FC_PATH/README.md"
+cp -r "$SCRIPT_DIR/../mod.cpp" "$FC_PATH/mod.cpp"
 
-cmd.exe /c start /D "$WIN_DZ_TOOL_PATH" AddonBuilder.exe "P:\FuelControl\scripts" "$WIN_FC_ADDONS_PATH" -project="P:\\" -clear
+cmd.exe /c start /D "$WIN_DZ_TOOL_PATH" AddonBuilder.exe "P:\FuelControl\scripts" "$WIN_FC_PATH\addons" -project="P:\\" -clear
 
